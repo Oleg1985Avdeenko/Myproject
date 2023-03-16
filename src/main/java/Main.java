0f0e37c5ccd7ec7c1f71fcb7150a62a281cd3d8c
Oleg.Avdeenko <93432136+Oleg1985Avdeenko@users.noreply.github.com>
2023-03-16 18:40:03 +0300

    16-03-2023 update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -27,60 +27,60 @@ public class Main {
 
         User user = new User();
         Client client = new Client();
-        Color color = new Color();
-        Engine engine = new Engine();
-        Model model = new Model();
-        ModelOption option = new ModelOption();
-        Transmission transmission = new Transmission();
+        Client client1 = new Client();
+        Client client2 = new Client();
         Car car = new Car();
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Model model = new Model();
+        Model model1 = new Model();
+        Model model2 = new Model();
+        Color color = new Color();
+        Color color1 = new Color();
+        Color color2 = new Color();
+        Color color3 = new Color();
+        Engine engine = new Engine();
+        Engine engine1 = new Engine();
+        Engine engine2 = new Engine();
+        Transmission transmission = new Transmission();
+        Transmission transmission1 = new Transmission();
+        Transmission transmission2 = new Transmission();
+        ModelOption option = new ModelOption();
+        ModelOption option1 = new ModelOption();
+        ModelOption option2 = new ModelOption();
         ClientOrder order = new ClientOrder();
+        ClientOrder order1 = new ClientOrder();
+        ClientOrder order2 = new ClientOrder();
 
         user.setLogin("root");
         user.setPassword("12345");
 
-//        client.setLogin("client1");
-//        client.setPassword("client11");
-//        client.setName("Oleg1");
-//        client.setSurname("Avdeenko1");
-//        client.setCellPhone("+375445323315");
-//        client.setEmail("Avdeenko-oleg@gmail.com");
         client = testData.getClient(client);
         client.getOrders().add(order);
 
-//        car.setPrice(new BigDecimal(75950));
-//        car.setAvailability(true);
+
         car = testData.getCar(car);
 
-//        model.setModelName("Atlas");
         model = testData.getModel(model);
 
         car.setCarModel(model);
         model.getSelectedModels().add(car);
 
-//        engine.setType("16 gas");
-//        engine.setVolume(1.6);
         engine = testData.getEngine(engine);
 
         car.setCarEngine(engine);
         engine.getSelectedEngines().add(car);
 
-//        color.setColorName("black");
         color = testData.getColor(color);
 
         car.setCarColor(color);
         color.getSelectedColors().add(car);
 
-//        transmission.setType("7 DCT");
         transmission = testData.getTransmission(transmission);
 
         car.setCarTransmission(transmission);
         transmission.getSelectedTransmissions().add(car);
 
-//        option.setSalon("Leather");
-//        option.setHeatedSeat(true);
-//        option.setCruiseControl(true);
-//        option.setSteeringWheelControl(true);
-//        option.setFogLight(true);
         option = testData.getOption(option);
 
         car.setCarOption(option);
@@ -90,8 +90,56 @@ public class Main {
         car.setSelectedCar(order);
         order.getSelectedCars().add(car);
 
+
+        client1 = testData.getClient1(client1);
+        client2 = testData.getClient2(client2);
+        car1 = testData.getCar1(car1);
+        car2 = testData.getCar2(car2);
+        model1 = testData.getModel1(model1);
+        model2 = testData.getModel2(model2);
+        car1.setCarModel(model1);
+        model1.getSelectedModels().add(car1);
+        car2.setCarModel(model2);
+        model2.getSelectedModels().add(car2);
+        engine1 = testData.getEngine1(engine1);
+        engine2 = testData.getEngine(engine2);
+        car1.setCarEngine(engine1);
+        engine1.getSelectedEngines().add(car1);
+        car2.setCarEngine(engine2);
+        engine2.getSelectedEngines().add(car2);
+        color1 = testData.getColor1(color1);
+        color2 = testData.getColor2(color2);
+        color3 = testData.getColor3(color3);
+        car1.setCarColor(color3);
+        color3.getSelectedColors().add(car1);
+        car2.setCarColor(color1);
+        color1.getSelectedColors().add(car2);
+        transmission1 = testData.getTransmission1(transmission1);
+        transmission2 = testData.getTransmission(transmission2);
+        car1.setCarTransmission(transmission1);
+        transmission1.getSelectedTransmissions().add(car1);
+        car2.setCarTransmission(transmission2);
+        transmission2.getSelectedTransmissions().add(car2);
+        option1 = testData.getOption1(option1);
+        option2 = testData.getOption(option2);
+        car1.setCarOption(option2);
+        option2.getSelectedOptions().add(car1);
+        car2.setCarOption(option1);
+        option1.getSelectedOptions().add(car2);
+        order1.setClient(client1);
+        car1.setSelectedCar(order1);
+        order1.getSelectedCars().add(car1);
+        order2.setClient(client2);
+        car2.setSelectedCar(order2);
+        order2.getSelectedCars().add(car2);
+
         userDAO.save(user);
         clientDAO.save(client);
+//        clientDAO.save(client1);
+//        clientDAO.save(client2);
+        carDAO.save(car1);
+        carDAO.save(car2);
+
 
     }
 }
--- src/main/java/TestData.java
+++ src/main/java/TestData.java
@@ -15,20 +15,63 @@ public class TestData {
         return client;
     }
 
+    public Client getClient1(Client client){
+        client.setLogin("client1");
+        client.setPassword("56789");
+        client.setName("Max");
+        client.setSurname("Petrukevich");
+        client.setCellPhone("+375445687258");
+        client.setEmail("Petrukevich@gmail.com");
+        return client;
+    }
+    public Client getClient2(Client client){
+        client.setLogin("client2");
+        client.setPassword("98765");
+        client.setName("Vitali");
+        client.setSurname("Barushko");
+        client.setCellPhone("+37544468825");
+        client.setEmail("Barushko@gmail.com");
+        return client;
+    }
+
     public Car getCar(Car car){
         car.setPrice(new BigDecimal(75950));
         car.setAvailability(true);
       return car;
     }
+        public Car getCar1(Car car){
+        car.setPrice(new BigDecimal(65875));
+        car.setAvailability(true);
+      return car;
+    }
+            public Car getCar2(Car car){
+        car.setPrice(new BigDecimal(54200));
+        car.setAvailability(true);
+      return car;
+    }
+
 
     public Model getModel(Model model){
         model.setModelName("Atlas");
         return model;
     }
+    public Model getModel1(Model model){
+        model.setModelName("CoolRay");
+        return model;
+    }
+    public Model getModel2(Model model){
+        model.setModelName("Emgrand");
+        return model;
+    }
 
     public Engine getEngine(Engine engine){
-        engine.setType("16 gas");
+        engine.setType("16 tdi");
         engine.setVolume(1.6);
+        return engine;
+    }
+    public Engine getEngine1(Engine engine){
+        engine.setType("1.5 L");
+        engine.setVolume(1.5);
         return engine;
     }
 
@@ -36,9 +79,25 @@ public class TestData {
         color.setColorName("black");
         return color;
     }
+    public Color getColor1(Color color){
+        color.setColorName("red");
+        return color;
+    }
+    public Color getColor2(Color color){
+        color.setColorName("green");
+        return color;
+    }
+    public Color getColor3(Color color){
+        color.setColorName("white");
+        return color;
+    }
 
     public Transmission getTransmission(Transmission transmission){
         transmission.setType("7 DCT");
+        return transmission;
+    }
+    public Transmission getTransmission1(Transmission transmission){
+        transmission.setType("CVT");
         return transmission;
     }
 
@@ -48,6 +107,14 @@ public class TestData {
         option.setCruiseControl(true);
         option.setSteeringWheelControl(true);
         option.setFogLight(true);
+        return option;
+    }
+    public ModelOption getOption1(ModelOption option) {
+        option.setSalon("Leather");
+        option.setHeatedSeat(true);
+        option.setCruiseControl(true);
+        option.setSteeringWheelControl(true);
+        option.setFogLight(false);
         return option;
     }
 }
